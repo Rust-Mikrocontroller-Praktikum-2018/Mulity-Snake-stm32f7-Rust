027a1691b60bc7c7d2e3100388ce67e3894a92cc
Unknown <wiesner.info@gmail.com>
2018-04-12 23:43:42 +0200

    Can receive data!

--- src/main.rs
+++ src/main.rs
@@ -144,50 +144,22 @@ fn main(hw: board::Hardware) -> ! {
         Ok(ether_device) => network = network::Network::new(ether_device, network_mode),
         Err(e) => panic!("error parsing ethernet_device: {:?}", e),
     }
-    network.operate();
 
     /* ETHERNET END */
 
     let random_gen = random::Random::new(rng, rcc);
     // Initialize Game
     let mut game = game::Game::new(graphics, i2c_3, random_gen);
-    gameloop(game);
+    gameloop(game, network);
 }
 
-fn gameloop(mut game: game::Game) -> ! {
-    // Define Pictures
-    // let pic1: &[u8] = include_bytes!("../assets/Welcom_screen/Snake_base2.bmp");
-    // let pic2: &[u8] = include_bytes!("../assets/Welcom_screen/Snake_mouth_open.bmp");
-    // let pic3: &[u8] = include_bytes!("../assets/Welcom_screen/Snake_mouth_shut.bmp");
-    // game.graphics
-    //     .print_bmp_at_with_rotaion(pic1, 0, 0, graphics::RotDirection::r_0);
-    // game.graphics.print_bmp_at_layer2(pic2, 300, 0);
-
-    // let welcome = "Welcome to Mulity-Snake! Touch Screen to start the Game\n";
-
-    // for c in welcome.chars() {
-    //     if c == ' ' || c == '-' || c == '!' {
-    //         print!("{}", c);
-    //         system_clock::wait(10);
-    //     } else {
-    //         game.graphics.print_bmp_at_downwards(pic2, 188, 85);
-    //         print!("{}", c);
-    //         //system_clock::wait(10);
-    //         game.graphics.print_bmp_at_downwards(pic3, 188, 85);
-    //     }
-    // }
-    // game.graphics.print_bmp_at_with_rotaion(pic, 0, 0, graphics::RotDirection::r_0);
-    // game.graphics.print_bmp_at_with_rotaion(pic, 85, 0, graphics::RotDirection::r_90);
-    // game.graphics.print_bmp_at_with_rotaion(pic, 170, 0, graphics::RotDirection::r_180);
-    // game.graphics.print_bmp_at_with_rotaion(pic, 260, 0, graphics::RotDirection::r_270);
-    /* Random Example */
-    let ran = game.random_gen.random_range(0, 42);
-    println!("A random number: 0 <= {} < 42!!!", ran);
+fn gameloop(mut game: game::Game, mut network: network::Network) -> ! {
     loop {
         // let ticks = system_clock::ticks();
-        game.move_snake();
-        game.snake_bite();
-        game.draw_game();
+        // game.move_snake();
+        // game.snake_bite();
+        // game.draw_game();
         system_clock::wait(100);
+        network.operate();
     }
 }
--- src/network.rs
+++ src/network.rs
@@ -1,18 +1,19 @@
 extern crate smoltcp;
 
-use alloc::Vec;
 use alloc::borrow::ToOwned;
+use alloc::Vec;
 use smoltcp::iface::EthernetInterface;
 // use smoltcp::phy::wait as phy_wait;
-use smoltcp::socket::{SocketSet, TcpSocket, TcpSocketBuffer};
+use smoltcp::socket::{Socket, SocketSet, UdpPacketMetadata, UdpSocket, UdpSocketBuffer};
 use smoltcp::time::Instant;
-use smoltcp::wire::{EthernetAddress, IpAddress, Ipv4Address};
+use smoltcp::wire::{EthernetAddress, IpAddress, IpEndpoint, Ipv4Address};
 use stm32f7::ethernet;
 use stm32f7::ethernet::EthernetDevice;
 use stm32f7::system_clock;
 
 /**
- * 
+ * How to use:
+  
     let mut network;
     // Todo: random EthernetAddress: FRAGE How to use random_gen here?
     let eth_addr = network::Network::get_ethernet_addr(network::NetworkMode::Client);
@@ -30,14 +31,14 @@ use stm32f7::system_clock;
         Ok(ether_device) => network = network::Network::new(ether_device, network::NetworkMode::Client),
         Err(e) => panic!("error parsing ethernet_device: {:?}", e),
     }
- *
+  
  * then use network inside a loop()
  */
 pub struct Network {
     ethernet_interface: EthernetInterface<'static, 'static, EthernetDevice>,
     network_mode: NetworkMode,
     sockets: SocketSet<'static, 'static, 'static>,
-    tcp_handle: smoltcp::socket::SocketHandle,
+    // tcp_handle: smoltcp::socket::SocketHandle,
     tcp_active: bool,
 }
 
@@ -60,29 +61,39 @@ impl Network {
     pub fn new(ethernet_device: EthernetDevice, network_mode: NetworkMode) -> Network {
         // Todo: Automatically choose ip/eth (maybe check if already there, or random?)
         let local_ip_addr: Ipv4Address;
+        let local_port: u16;
         match network_mode {
-            NetworkMode::Server => local_ip_addr = Ipv4Address([192, 168, 0, 24]),
-            NetworkMode::Client => local_ip_addr = Ipv4Address([192, 168, 0, 42]),
+            NetworkMode::Server => {
+                local_ip_addr = Ipv4Address([192, 168, 0, 24]);
+                local_port = 2424;
+            }
+            NetworkMode::Client => {
+                local_ip_addr = Ipv4Address([192, 168, 0, 42]);
+                local_port = 4242
+            }
         }
+        let local_endpoint = IpEndpoint::new(IpAddress::Ipv4(local_ip_addr), local_port);
+
+        let ethernet_interface = ethernet_device.into_interface(local_ip_addr);
 
         // let remote_ip_addr = IpAddress::v4(192, 168, 0, 50);
         // let remote_port = 50000_u16;
 
-        let tcp_rx_buffer = TcpSocketBuffer::new(vec![0; ethernet::MTU]);
-        let tcp_tx_buffer = TcpSocketBuffer::new(vec![0; ethernet::MTU]);
-        let tcp_socket = TcpSocket::new(tcp_rx_buffer, tcp_tx_buffer);
-
-        let ethernet_interface = ethernet_device.into_interface(local_ip_addr);
+        let udp_rx_buffer = UdpSocketBuffer::new(vec![UdpPacketMetadata::EMPTY; 3], vec![0u8; 256]);
+        let udp_tx_buffer = UdpSocketBuffer::new(vec![UdpPacketMetadata::EMPTY; 1], vec![0u8; 128]);
+        let mut udp_socket = UdpSocket::new(udp_rx_buffer, udp_tx_buffer);
+        udp_socket.bind(local_endpoint).unwrap();
 
         // tcp_socket.listen(endpoint).unwrap();
         let mut sockets = SocketSet::new(Vec::new());
-        let tcp_handle = sockets.add(tcp_socket);
+        /* let tcp_handle =  */
+        sockets.add(udp_socket);
 
         Network {
             ethernet_interface: ethernet_interface,
             network_mode: network_mode,
             sockets: sockets,
-            tcp_handle: tcp_handle,
+            // tcp_handle: tcp_handle,
             tcp_active: false,
         }
     }
@@ -97,7 +108,7 @@ impl Network {
         }
     }
 
-    fn print_data_as_char(data: &Vec<u8>) {
+    fn print_data_as_char(data: &mut Vec<u8>) {
         if data.len() > 0 {
             // data = data.split(|&b| b == b'\n').collect::<Vec<_>>().concat();
             hprint!("Data received: ");
@@ -109,7 +120,7 @@ impl Network {
     }
 
     fn operate_server(&mut self) {
-        let local_port = 6969_u16;
+        /* let local_port = 6969_u16;
         // let timestamp = Instant::now();
         let timestamp = Instant::from_millis(system_clock::ticks() as i64);
         self.ethernet_interface
@@ -134,7 +145,7 @@ impl Network {
                 let data = socket
                     .recv(|buffer| {
                         let data = buffer.to_owned();
-                        Network::print_data_as_char(&data);
+                        // Network::print_data_as_char(&data);
                         (data.len(), data)
                     })
                     .unwrap();
@@ -151,67 +162,61 @@ impl Network {
                 hprintln!("tcp:6970 close");
                 socket.close();
             }
-        }
+        } */
         // phy_wait(fd, iface.poll_delay(&sockets, timestamp)).expect("wait error");
     }
 
     fn operate_client(&mut self) {
-        {
-            let mut socket = self.sockets.get::<TcpSocket>(self.tcp_handle);
-            if !socket.is_open() {
-                let remote_ip_addr = IpAddress::v4(192, 168, 0, 50);
-                let remote_port = 6969_u16;
-                let local_port = 7454_u16;
-                socket
-                    .connect((remote_ip_addr, remote_port), local_port)
-                    .unwrap();
-            }
-        }
-
-        {
-            // let timestamp = Instant::now();
-            let timestamp = Instant::from_millis(system_clock::ticks() as i64);
-            self.ethernet_interface
-                .poll(&mut self.sockets, timestamp)
-                .expect("poll error");
-        }
-
-        {
-            let mut socket = self.sockets.get::<TcpSocket>(self.tcp_handle);
-            if socket.is_active() && !self.tcp_active {
-                hprintln!("connected");
-            } else if !socket.is_active() && self.tcp_active {
-                hprintln!("disconnected");
-                // break;
-                return;
-            }
-            self.tcp_active = socket.is_active();
-
-            if socket.may_recv() {
-                /* let data =  */
-                socket
-                    .recv(|buffer| {
-                        let data = buffer.to_owned();
-                        Network::print_data_as_char(&data);
-                        (data.len(), data)
-                    })
-                    .unwrap();
-                if socket.can_send()
-                /* && data.len() > 0 */
-                {
-                    hprintln!("send data: pong");
-                    // socket.send_slice(&data[..]).unwrap();
-                    let pong_slice = vec!['p' as u8, 'o' as u8, 'n' as u8, 'g' as u8];
-                    let pong_slice = pong_slice.as_slice();
-                    socket.send_slice(pong_slice).unwrap();
-                    // write!(socket, "pong\n").unwrap();
+        // let timestamp = Instant::now();
+        let timestamp = Instant::from_millis(system_clock::ticks() as i64);
+        match self.ethernet_interface.poll(&mut self.sockets, timestamp) {
+            Err(::smoltcp::Error::Exhausted) => return,
+            Err(::smoltcp::Error::Unrecognized) => {}
+            Err(e) => println!("Network error: {:?}", e),
+            Ok(socket_changed) => if socket_changed {
+                for mut socket in self.sockets.iter_mut() {
+                    Network::poll_socket(&mut socket).expect("socket poll failed");
                 }
-            } else if socket.may_send() {
-                // (könnte) may send -> line is empty -> nothing to listen?
-                hprintln!("close");
-                socket.close();
-            }
+            },
         }
+
+        // {
+        //     let mut socket = self.sockets.get::<TcpSocket>(self.tcp_handle);
+        //     if socket.is_active() && !self.tcp_active {
+        //         hprintln!("connected");
+        //     } else if !socket.is_active() && self.tcp_active {
+        //         hprintln!("disconnected");
+        //         // break;
+        //         return;
+        //     }
+        //     self.tcp_active = socket.is_active();
+
+        //     if socket.may_recv() {
+        //         hprintln!("socket.may_recv");
+        //         /* let data =  */
+        //         socket
+        //             .recv(|buffer| {
+        //                 let data = buffer.to_owned();
+        //                 Network::print_data_as_char(&data);
+        //                 (data.len(), data)
+        //             })
+        //             .unwrap();
+        //         if socket.can_send()
+        //         /* && data.len() > 0 */
+        //         {
+        //             hprintln!("send data: pong");
+        //             // socket.send_slice(&data[..]).unwrap();
+        //             let pong_slice = vec!['p' as u8, 'o' as u8, 'n' as u8, 'g' as u8];
+        //             let pong_slice = pong_slice.as_slice();
+        //             socket.send_slice(pong_slice).unwrap();
+        //             // write!(socket, "pong\n").unwrap();
+        //         }
+        //     } else if socket.may_send() {
+        //         // (könnte) may send -> line is empty -> nothing to listen?
+        //         hprintln!("close");
+        //         socket.close();
+        //     }
+        // }
 
         // phy_wait(fd, self.ethernet_interface.poll_delay(&self.sockets, timestamp)).expect("wait error");
     }
@@ -220,6 +225,29 @@ impl Network {
         match network_mode {
             NetworkMode::Client => EthernetAddress([0x00, 0x08, 0xdc, 0xab, 0xcd, 0xef]),
             NetworkMode::Server => EthernetAddress([0x00, 0x08, 0xdc, 0xab, 0xcd, 0xf0]),
+        }
+    }
+
+    fn poll_socket(socket: &mut Socket) -> Result<Option<Vec<u8>>, smoltcp::Error> {
+        match socket {
+            &mut Socket::Udp(ref mut socket) => match socket.endpoint().port {
+                // Client Port
+                4242_u16 => match socket.recv() {
+                    Ok((data, _remote_endpoint)) => {
+                        let mut data_copy = data.to_owned();
+                        Network::print_data_as_char(&mut data_copy);
+                        Ok(Some(Vec::from(data)))
+                    }
+                    Err(smoltcp::Error::Exhausted) => Ok(None),
+                    Err(err) => Err(err),
+                },
+                _ => {
+                    Ok(None)
+                }
+            },
+            _ => {
+                Ok(None)
+            }
         }
     }
 }
